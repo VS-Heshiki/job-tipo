a8cd07448d91a7ba6b9b6d03f130a1ecc487839e
Victor Heshiki <57600375+hunzptk@users.noreply.github.com>
2021-06-06 23:45:23 -0300

    Update scripts.php

--- api/src/scripts.php
+++ api/src/scripts.php
@@ -5,7 +5,8 @@
   crossorigin="anonymous">
 </script>
 <script src="./OwlCarousel/dist/owl.carousel.min.js"></script>
-<script src="./scripts/menu.js"></script>
+<script src="./OwlCarousel/dist/owl.carousel.min.js"></script>
+<script src="./scripts/owl-carousel.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
 integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" 
 crossorigin="anonymous"></script>
